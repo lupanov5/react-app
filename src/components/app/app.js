683a8d36c81081a84c4926dae13c52192447dd09
Dmitry <lupanov5@mail.ru>
2021-08-19 16:16:13 +0300

    Реализовал поиск по постам, фильтры. Окончательная версия приложения

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -16,13 +16,17 @@ export default class App extends Component {
                 {label: 'Going to learn React', important: true, like: false, id: 1},
                 {label: 'That is so good', important: false, like: false, id: 2},
                 {label: 'I need a break...', important: false, like: false, id: 3}
-            ]
+            ],
+            term: '',
+            filter: 'all'
         };
 
         this.deleteItem = this.deleteItem.bind(this);
         this.addItem = this.addItem.bind(this);
         this.onToggleImportant = this.onToggleImportant.bind(this);
         this.onToggleLiked = this.onToggleLiked.bind(this);
+        this.onUpdateSearch = this.onUpdateSearch.bind(this);
+        this.onFilterSelect = this.onFilterSelect.bind(this);
 
         this.maxId = 4;
     }
@@ -85,11 +89,39 @@ export default class App extends Component {
         }) 
     }
 
+    searchPost(items, term) {
+        if (term.length === 0) {
+            return items;
+        }
+
+        return items.filter((item) => {
+            return item.label.indexOf(term) > -1;
+        })
+    }
+
+    onFilterPost(items, filter) { 
+        if (filter === 'like') {
+            return items.filter(item => item.like);
+        } else {
+            return items;
+        }
+    }
+
+    onUpdateSearch(term) {
+        this.setState({term});
+    }
+
+    onFilterSelect(filter) {
+        this.setState({filter});
+    }
+
     render() {
 
-        const {data} = this.state;
+        const {data, term, filter} = this.state;
         const liked = data.filter(item => item.like).length; //фильтруем все посты и записываем в переменную общее количество лайков.
         const allPosts = data.length; //общее количество постов.
+
+        const visiblePosts = this.onFilterPost(this.searchPost(data, term), filter);
 
         return (
             <div className='app'>
@@ -97,11 +129,14 @@ export default class App extends Component {
                 liked={liked}
                 allPosts={allPosts}/>
                 <div className="search-panel d-flex">
-                    <SearchPanel/>
-                    <PostStatusFilter/>
+                    <SearchPanel
+                    onUpdateSearch={this.onUpdateSearch}/>
+                    <PostStatusFilter
+                    filter={filter}
+                    onFilterSelect={this.onFilterSelect}/>
                 </div>
                 <PostList
-                    posts={this.state.data}
+                    posts={visiblePosts}
                     onDelete={this.deleteItem}
                     onToggleImportant={this.onToggleImportant}
                     onToggleLiked={this.onToggleLiked}/>
--- src/components/post-status-filter/post-status-filter.js
+++ src/components/post-status-filter/post-status-filter.js
@@ -1,15 +1,38 @@
-import React from 'react'
+import React, { Component } from 'react'
 import { Button } from 'reactstrap';
 
 import './post-status-filter.css'
 
-const PostStatusFilter = () => {
-    return (
-        <div className="btn-group">
-            <Button outline color="primary">Все</Button>
-            <Button outline color="warning">Понравилось</Button>
-        </div>
-    )
-}
+export default class PostStatusFilter extends Component{
+    constructor(props) {
+        super(props);
+        this.buttons = [
+            {name: 'all', label: 'Все'},
+            {name: 'like', label: 'Понравилось'}
+        ]
+        
+    }
+    
+    render() {
 
-export default PostStatusFilter;
+        const buttons = this.buttons.map(({name, label}) => {
+            const {filter, onFilterSelect} = this.props;
+            const active = filter === name;
+            const clazz = active ? 'btn-info' : 'btn-outline';
+            return (
+                <Button
+                    key={name}
+                    type="button"
+                    className={`btn ${clazz}`}
+                    onClick={() => onFilterSelect(name)}>
+                {label}</Button>
+            )
+        })
+
+        return (
+            <div className="btn-group">
+                {buttons}
+            </div>
+        )
+    }
+}
